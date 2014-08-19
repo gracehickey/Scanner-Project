ebce15a00bf2018268ec9542ca185e253902ff75
Grace.Hickey <Grace.Hickey@MU-C3-11.mullen.pvt.k12.co.us>
2014-08-19 13:12:42 -0600

    Initial Commit

---
+++ Scanner Practice/src/ScannerProject2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ScannerProject2 {
+
+	public static void main(String[] args) {
+		Scanner fred = new Scanner(System.in);
+			int apple;
+			double pan;
+			String sink;
+		System.out.println("Please enter your favoirte integer.");
+			apple = fred.nextInt();
+			if(apple > 9)
+			{
+				System.out.println("happy");
+			}
+			if(apple < 9)
+				{
+				System.out.println("Me too!");
+				}
+			
+
+	}
+
+}
